8a21d6d8c513752f189e73f24f7ed4cdf019b380
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-10-25 13:32:38 +0800

    版本号更新、介绍优化

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ with open("README.md", encoding="utf-8") as f:
 setup(
 	name="ffmpeg-media-converter",
 	description="Batch media file format converter based on ffmpeg.",
-	version="0.2.0",
+	version="0.2.1",
 	packages=find_packages(),
 	url="https://github.com/lc6464/ffmpeg-media-converter",
-	license="Apache License",
+	license="Apache License 2.0",
 	long_description=long_description,
 	long_description_content_type="text/markdown"
 )
